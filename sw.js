b88b6116c98b3a34d2ecd176e4a243f776bba0b9
Victor Carvalho <victor.carvalhok23@gmail.com>
2024-11-08 14:47:49 -0400

    update
    pwa funcionando

---
+++ sw.js
@@ -0,0 +1,37 @@
+let cacheName = "delicias-da-cozinha-v1";
+let filesToCache = [ '/',
+    '/index.html',
+    '/massas.html',
+    '/doces.html',
+    '/carnes.html',
+    '/css/styles.css',
+    '/js/app.js',
+    '/images/espaguete.jpg',
+    '/images/brigadeiro.jpg',
+    '/images/bife_parmegiana.jpg',
+    '/images/costela.jpg',
+    '/images/frango_curry.jpg',
+    '/images/lasanha.jpg',
+    '/images/pudim.jpg',
+    '/images/ravioli.jpg',
+    '/images/torta_limao.jpg'];
+
+/* inicializando a service worker e fazendo o 
+download do conteúdo da aplicação */
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* disponibilizando o conteudo quando estiver offline */
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
